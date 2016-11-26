8307fbd6f555ae0cf19e87de8c6f530c37f9d081
thymajesty <yesthymajesty@gmail.com>
2016-11-27 00:11:28 +0200

    build-static

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,14 +1,17 @@
-switch (process.env.NODE_ENV) {
+let argument = process.env.NODE_ENV && process.env.NODE_ENV.trim();
+
+switch (argument) {
     case 'prod':
     case 'production':
         module.exports = require('./config/webpack.prod');
         break;
-    case 'test':
-    case 'testing':
-        module.exports = require('./config/webpack.test');
+    case 'static':
+        module.exports = require('./config/webpack.build-static');
         break;
     case 'dev':
     case 'development':
     default:
         module.exports = require('./config/webpack.dev');
 }
+
+

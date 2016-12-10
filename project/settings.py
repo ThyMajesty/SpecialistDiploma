f76159294c63f802a350a1a167c18c78d942967e
POD666 <v.v.danyliuk@gmail.com>
2016-12-10 09:05:17 +0200

    tmp no auth

--- project/settings.py
+++ project/settings.py
@@ -321,8 +321,5 @@ TASTYPIE_ABSTRACT_APIKEY = True
 JWT_AUTH_HEADER_PREFIX = 'JWT'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
     'PAGE_SIZE': 10
 }

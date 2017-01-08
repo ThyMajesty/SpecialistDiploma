d1053d8b12d805deffcd1d38e843c205734e374f
POD666 <v.v.danyliuk@gmail.com>
2017-01-08 08:13:50 +0200

    uhiuih

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -6,6 +6,6 @@ from .views import index, oauth_callback, FileUploadView, MultiFileUploadView
 urlpatterns = [
     url(r'^$', index),
     url(r'^oauth_callback/', oauth_callback),
-    url(r'^upload/$', FileUploadView.as_view())
+    url(r'^upload/$', FileUploadView.as_view()),
     url(r'^uploads/$', MultiFileUploadView.as_view())
 ]

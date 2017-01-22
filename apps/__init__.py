4dcf33959cf2f0c2d7cb370920b1ae9959b6765d
POD666 <v.v.danyliuk@gmail.com>
2017-01-22 22:43:44 +0200

    qwe

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,9 +1,1 @@
-from social.strategies.django_strategy import DjangoStrategy
-from django.http import JsonResponse
 
-
-def redirect(self, url):
-    return JsonResponse({'url': url})
-
-
-DjangoStrategy.redirect = redirect

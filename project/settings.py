84c50ce76d1d8cf5e49efb4e5f435d36192eb0aa
POD666 <v.v.danyliuk@gmail.com>
2017-01-24 00:05:33 +0200

    sdfsdf

--- project/settings.py
+++ project/settings.py
@@ -206,7 +206,7 @@ JWT_AUTH = {
     'JWT_PRIVATE_KEY': None,
     'JWT_ALGORITHM': 'HS256',
     'JWT_VERIFY': True,
-    'JWT_VERIFY_EXPIRATION': not DEBUG,
+    'JWT_VERIFY_EXPIRATION': False,
     'JWT_LEEWAY': 0,
     'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
     'JWT_AUDIENCE': None,

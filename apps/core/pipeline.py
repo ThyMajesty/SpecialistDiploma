2d18cf007861c8dc0b433a56c07ea3120612dd2c
POD666 <v.v.danyliuk@gmail.com>
2017-01-23 23:45:34 +0200

    sdf

--- apps/core/pipeline.py
+++ apps/core/pipeline.py
@@ -9,14 +9,19 @@ def update_user_neo4j_record(backend, user, response, *args, **kwargs):
     except Person.DoesNotExist:
         person = Person(user_id=user.pk)
         data = dict(response)
-        args = {
-            'name': data.get('displayName', ''),
-            'image': data.get('image', {}).get('url', None),
-            'email': data.get('emails', [{}])[0].get('value', None),
-            "socialAccounts": {
-                'url': data.get('url', ''),
-                'name': backend.name
+        if backend.name == 'google-oauth2':
+            args = {
+                'name': data.get('displayName', ''),
+                'image': data.get('image', {}).get('url', None),
+                'email': data.get('emails', [{}])[0].get('value', None),
+                "socialAccounts": {
+                    'url': data.get('url', ''),
+                    'name': backend.name
+                }
             }
-        }
+        elif backend.name == 'twitter':
+            raise Exception(data)
+        elif backend.name == 'facebook':
+            raise Exception(data)
         person.value = json.dumps(args)
         person.save()

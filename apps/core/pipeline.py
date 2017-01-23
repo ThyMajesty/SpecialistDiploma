37cb2d9b1cbd4da0c8b75eeb2395ffd4c956343d
POD666 <v.v.danyliuk@gmail.com>
2017-01-24 00:01:10 +0200

    awea

--- apps/core/pipeline.py
+++ apps/core/pipeline.py
@@ -11,7 +11,7 @@ def update_user_neo4j_record(backend, user, response, *args, **kwargs):
         data = dict(response)
         if backend.name == 'google-oauth2':
             args = {
-                'name': data.get('displayName', ''),
+                'name': data.get('displayName', user.username),
                 'image': data.get('image', {}).get('url', None),
                 'email': data.get('emails', [{}])[0].get('value', None),
                 "socialAccounts": {
@@ -20,8 +20,21 @@ def update_user_neo4j_record(backend, user, response, *args, **kwargs):
                 }
             }
         elif backend.name == 'twitter':
-            raise Exception(data)
+            args = {
+                'name': data.get('name', user.username),
+                'image': data.get('profile_image_url', None),
+                "socialAccounts": {
+                    'id': data.get('user_id', None),
+                    'name': backend.name
+                }
+            }
         elif backend.name == 'facebook':
-            raise Exception(data)
+            args = {
+                'name': data.get('name', user.username),
+                "socialAccounts": {
+                    'id': data.get('id', None),
+                    'name': backend.name
+                }
+            }
         person.value = json.dumps(args)
         person.save()
